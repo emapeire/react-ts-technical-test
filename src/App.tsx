3eb369b8feba445891a5384c106d59021cc75f58
Emanuel Peire <emapeire@outlook.com>
2023-11-28 16:25:25 -0300

    Add sorting functionality to UserList component

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,21 @@ import UserList from './components/UserList'
 function App() {
   const [users, setUsers] = useState<Users[]>([])
   const [showColors, setShowColors] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
 
   const toggleColor = () => {
     setShowColors(!showColors)
   }
+
+  const toogleSortByCountry = () => {
+    setSortByCountry((prevState) => !prevState)
+  }
+
+  const sortedUsers = sortByCountry
+    ? [...users].sort((a, b) => {
+        return a.location.country.localeCompare(b.location.country)
+      })
+    : users
 
   useEffect(() => {
     fetch(`${API_URL}`)
@@ -22,13 +33,20 @@ function App() {
   }, [])
 
   return (
-    <div className='App'>
+    <div>
       <h1>User Table</h1>
-      <header>
-        <button onClick={toggleColor}>Color rows</button>
+      <header
+        style={{ display: 'flex', gap: '1rem', justifyContent: 'center' }}
+      >
+        <button onClick={toggleColor}>
+          {showColors ? 'Uncolor rows' : 'Color rows'}
+        </button>
+        <button onClick={toogleSortByCountry}>
+          {sortByCountry ? 'Unsort by country' : 'Sort by country'}
+        </button>
       </header>
       <main>
-        <UserList showColors={showColors} users={users} />
+        <UserList showColors={showColors} users={sortedUsers} />
       </main>
     </div>
   )
